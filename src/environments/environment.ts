5d6789c4a8b0a4ec67edb6aee41305e48500cf90
Teddie <teddymachoka98@yahoo.com>
2020-08-31 09:18:50 +0300

    add changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: '395037ee948bb09cbc1e0510d913456b806eef4d'
 };
 
 /*
